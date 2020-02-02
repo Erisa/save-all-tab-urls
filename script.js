6a4223c176316c12a19d7777f4dd057c9d3dbdbd
Matthew Baa <matthewbaa@tutanota.com>
2020-02-02 19:46:25 +0000

    Implement save to file functionality, closes #2

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-mainTextarea = document.getElementById("main-textarea");
+var mainTextarea = document.getElementById("main-textarea");
+var downloadLink = document.getElementById("download-link");
+var tabURLs = []; // Need to refactor to not have this in public scope.
 
-browser.tabs.query({active: false}, tabs => { // Don't save the tab that the user is currently on, which is this extension's tab.
-  var tabURLs = [];
+function updateTabURLs() {
+  browser.tabs.query({active: false}, tabs => { // Don't save the tab that the user is currently on, which is this extension's tab.
+    tabs.forEach(tab => {
+        tabURLs.push(tab.url);
+    });
 
-  tabs.forEach(tab => {
-      tabURLs.push(tab.url);
+    mainTextarea.value = tabURLs.join("\n");
   });
+}
 
-  mainTextarea.value = tabURLs.join("\n");
+downloadLink.addEventListener("click", () => {
+  var data = mainTextarea.value; // Can't re-run updateTabURLs() as user may have edited the text.
+  var filename = "saved-tabs_" + new Date().toISOString().replace(/:/g, "-").replace("T", "_").slice(0, 19); // Regex replaces all occurences.
+  
+  createDownloadFile(data, filename, "text/plain");
 });
+
+function createDownloadFile(data, filename, type) {
+  var file = new Blob([data], {type: type});
+  
+  downloadLink.href = URL.createObjectURL(file);
+  downloadLink.download = filename;
+}
+
+updateTabURLs();
+createDownloadFile();
